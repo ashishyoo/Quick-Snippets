870f665da8aecb4b177057388a4cac67852bbe2f
Ashish Shrestha <ashish.shrestha@truenary.com>
2025-06-24 14:38:53 +0545

    refactor: add minor validations for inputs

--- src/types/form.schema.ts
+++ src/types/form.schema.ts
@@ -1,9 +1,10 @@
 import { z } from "zod";
 
 export const schema = z.object({
-  name: z.string().min(1, "Name is required"),
+  name: z.string().trim().min(1, "Name is required"),
   snippet: z
     .string()
+    .trim()
     .min(1, "Snippet is required")
     .max(800, "Maximum limit excedded"),
 });
