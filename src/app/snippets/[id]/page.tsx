eb6897bf074ef1eeda7fb0dd828cb6ccdf3e555b
Ashish Shrestha <ashish.shrestha@truenary.com>
2025-06-24 17:46:08 +0545

    refactor: improve styling

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -2,7 +2,7 @@ import SnippetDetail from "@/components/SnippetDetail";
 
 const page = () => {
   return (
-    <div className="min-h-screen w-full flex items-center justify-center relative">
+    <div className="min-h-screen w-full flex items-center justify-center relative p-3">
       <SnippetDetail />
     </div>
   );
