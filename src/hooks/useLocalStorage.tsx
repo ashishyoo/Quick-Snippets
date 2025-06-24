3c158cfdc41bcbd8813b1f0aa8dea418d065e8de
Ashish Shrestha <ashish.shrestha@truenary.com>
2025-06-24 14:37:52 +0545

    feat: create custom hook for local storage

---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,10 @@
+import useSnippetStore from "@/state/store";
+import { saveToLocal } from "@/utils/localStorage";
+import { useEffect } from "react";
+
+const useLocalStorage = () => {
+  const { snippets } = useSnippetStore();
+  useEffect(() => saveToLocal(snippets), [snippets]);
+};
+
+export default useLocalStorage;
