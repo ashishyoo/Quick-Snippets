bd524c532fed8f0d76ce2564fd93ef90e52055b5
Ashish Shrestha <ashish.shrestha@truenary.com>
2025-06-25 11:05:31 +0545

    refactor: change the layouts

--- src/app/snippets/page.tsx
+++ src/app/snippets/page.tsx
@@ -1,12 +1,8 @@
-import { Typography } from "@mui/material";
 import Snippets from "@/components/Snippets";
 
 const page = () => {
   return (
     <div className="p-6">
-      <Typography variant="h3" className="pb-6">
-        All Snippets
-      </Typography>
       <Snippets />
     </div>
   );
